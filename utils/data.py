c7c8f723c44b6a286dbb8a27a1653af4341094a6
LEONARDOSUSA <leonardosusa@hotmail.com>
2025-06-30 12:11:57 -0400

    data.py

--- utils/data.py
+++ utils/data.py
@@ -2,17 +2,17 @@ import os
 import pandas as pd
 import alpaca_trade_api as tradeapi
 
+# Configuración Alpaca
 ALPACA_KEY = os.getenv("ALPACA_KEY")
 ALPACA_SECRET = os.getenv("ALPACA_SECRET")
 BASE_URL = "https://paper-api.alpaca.markets"
-
 api = tradeapi.REST(ALPACA_KEY, ALPACA_SECRET, base_url=BASE_URL)
 
-def obtener_datos(ticker, limit=100, timeframe="5Min"):
+def obtener_datos(ticker, limit=150, timeframe="5Min"):
     try:
         bars = api.get_bars(symbol=ticker, timeframe=timeframe, limit=limit).df
         df = bars[['open', 'high', 'low', 'close', 'volume']].copy()
-        df = df.reset_index(drop=True)
+        df.index = df.index.tz_convert('America/New_York')
         print(f"✅ Datos cargados para {ticker} ({len(df)} velas)", flush=True)
         return df
     except Exception as e:
